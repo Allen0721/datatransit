54a54a49ecbe84e3087d80fca5eb07e7dde99a03
zhaohuating <zhaoht@zhihenget.com>
2017-06-16 11:16:28 +0800

    # add: 添加配置文件

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+      timeOut : 5, // 连接超时时间 秒
+      reconnectionTime : 30, //连接失败后重新连接时间  秒
+      serverHost : '192.168.88.27', // 服务器ip地址
+      serverPort : '5527',      //服务器端口
+      clientHost : 'localhost',//客户端ip 本地ip
+      clientPort : '5526',    //客户端端口 本地端口
+      username : 'admin', //用户名 远程服务器连接用户名
+      password : '123456', // 密码 远程服务器连接密码
+
+      filepath : 'D:/logs/'  //文件保存路径
+}
